08802e718e1a0bc07a26b500a4d4f11b63e3f673
romrobotics <server01.psa1981@gmail.com>
2021-11-27 11:59:57 +0630

    edit scripts

--- scripts/2_install_ros_noetic.bash
+++ scripts/2_install_ros_noetic.bash
@@ -4,15 +4,23 @@ echo ""
 echo "          ROM ROBOTICS LTD     "
 echo ""
 
+
 # DELETE user
 sudo pkill -u ubuntu
 sudo userdel -r ubuntu
 
+
 # UPDATE, UPGRADE
-echo " ROM ROBOTICS LTD:: updating ....."
+echo ""
+echo " ROM ROBOTICS LTD:: updating ......................................................."
+echo ""
 sudo apt update 
 
+
 # INSTALL ROS NOETIC
+echo ""
+echo " ROM ROBOTICS LTD:: installing ros noetic ( base, robot, navigation) ..............."
+echo ""
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
@@ -23,16 +31,25 @@ sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generat
 sudo rosdep init
 rosdep update
 
+
 # CREATE WORKSPACE
-mkdir -p ROS/catkin_ws/src
-cd ROS/catkin_ws/src
+echo ""
+echo " ROM ROBOTICS LTD:: Creating workspace /ROS/catkin_ws/src ........................."
+echo ""
+source ~/.bashrc
+mkdir -p ~/ROS/catkin_ws/src
+cd ~/ROS/catkin_ws/src
 catkin_init_workspace
-cd ..
+cd ~/ROS/catkin_ws
 catkin_make
 echo "source /home/ROS/catkin_ws/devel/setup.bash">> ~/.bashrc
 
+
 # SET TIME ZONE AND REBOOT
+echo ""
+echo " ROM ROBOTICS LTD:: Time synchronize ........................."
+echo ""
 sudo timedatectl set-timezone Asia/Yangon
 sudo timedatectl set-ntp true
-
+date
 # Nowaday, timedatectl is replaced with NPT, 
--- scripts/3_install_dependencies.bash
+++ scripts/3_install_dependencies.bash
@@ -5,34 +5,47 @@ echo "          ROM ROBOTICS LTD     "
 echo ""
 
 # UPDATE, UPGRADE
-echo " ROM ROBOTICS LTD:: updating ....."
+echo ""
+echo " ROM ROBOTICS LTD:: updating ...................................."
+echo ""
 source ~/.bashrc
 sudo apt update && sudo apt upgrade
 
 # INSTALL ROS PACKAGES
+echo ""
 echo " ROM ROBOTICS LTD:: installing required ros packages ............"
+echo ""
 sudo apt install ros-noetic-depthimage-to-laserscan
 # git clone 
 cd ~/ROS/catkin_ws/ && catkin_make
 
 # CONFIGURE NETWORK
+echo ""
 echo " ROM ROBOTICS LTD:: writing Network Setting to ~/.bashrc ........"
+echo ""
 echo "export ROS_IP=192.169.100.158" >> ~/.bashrc
 echo "export ROS_HOSTNAME=192.168.100.158" >> ~/.bashrc
 echo "export ROS_MASTER_URI=http://192.168.100.57:11311" >> ~/.bashrc
 
 # SOURCE BASHRC
+echo ""
 echo " ROM ROBOTICS LTD:: sourcing .bashrc to ~/.profile .............."
+echo ""
 echo "if [ -f ~/.bashrc ]; then " >> ~/.profile
 echo "  . ~/.bashrc " >> ~/.profile
 echo "fi " >> ~/.profile
 
 # INSTALL UDEV RULES
+echo ""
 echo " ROM ROBOTICS LTD:: install udev rules .........................."
+echo ""
 sudo cp `rospack find rom2109_controller`/udev/rplidar.rules /etc/udev/rules.d
 sudo ln -s /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0 ~/robotController
 
 # COMPILE ROBOT DRIVER
+echo ""
+echo " ROM ROBOTICS LTD:: Trying to ROM2109 ROBOT DRIVER ..............."
+echo ""
 cp -r ~/rom2109 ~/ROS/catkin_ws/src
 cd ~/ROS/catkin_ws/
 catkin_make
