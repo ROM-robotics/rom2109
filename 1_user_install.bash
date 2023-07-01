e4a08d3f074384901674cb4d371fbc4f3be5f1d9
ROM Robotics <server01.psa1981@gmail.com>
2023-07-01 12:15:58 +0630

    Update 1_user_install.bash

--- 1_user_install.bash
+++ 1_user_install.bash
@@ -3,7 +3,8 @@ sudo apt install openssh-server
 sudo systemctl enable ssh.service
 
 sudo useradd -md rom
-echo "Enter password for user rom"
+echo "User name : rom"
+echo "Enter password : "
 passwd rom
 
 sudo userdel -rf ubuntu
