6159f9cffbff5289eb65572bd0befd8ea8c4d94a
Pyae Soan Aung <server01.psa1981@gmail.com>
2023-11-15 00:02:30 +0630

    nov15 untest

--- rom2109_controller/launch/rom2109.launch.py
+++ rom2109_controller/launch/rom2109.launch.py
@@ -58,10 +58,10 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
-    robot_controller_spawner = Node(
+    base_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diffbot_base_controller", "--controller-manager", "/controller_manager"],
+        arguments=["base_controller", "--controller-manager", "/controller_manager"],
     )
 
     # Delay rviz start after `joint_state_broadcaster`
@@ -73,10 +73,10 @@ def generate_launch_description():
     )
 
     # Delay start of robot_controller after `joint_state_broadcaster`
-    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+    delay_base_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=joint_state_broadcaster_spawner,
-            on_exit=[robot_controller_spawner],
+            on_exit=[base_controller_spawner],
         )
     )
 
@@ -85,7 +85,7 @@ def generate_launch_description():
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
         #delay_rviz_after_joint_state_broadcaster_spawner,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        delay_base_controller_spawner_after_joint_state_broadcaster_spawner,
     ]
 
     return LaunchDescription(nodes)
--- rom2109_simple_commander/rom2109_simple_commander/README.md
+++
@@ -1,1 +0,0 @@
-pip install setuptools==58.2.0
